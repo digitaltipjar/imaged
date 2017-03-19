403edc4e30650b021f06d3f3260f79d9ca00e2c2
Bryan Joseph <bryanjos@gmail.com>
2017-03-19 15:55:47 -0500

    Update to node 7.7.2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node lib/index.js
+web: node src/index.js
--- src/index.js
+++ src/index.js
@@ -1,91 +1,105 @@
-import express from 'express';
-import AWS from 'aws-sdk';
-import gm from 'gm';
-import uuid from 'node-uuid';
-import fs from 'fs';
-import multer from 'multer';
+const express = require('express');
+const AWS = require('aws-sdk');
+const gm = require('gm');
+const fs = require('fs');
+const multer = require('multer');
 
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: 'uploads/' });
 
-const s3 = new AWS.S3({region: 'us-east-1'});
+const s3 = new AWS.S3({ region: 'us-east-1' });
 
 const gmObj = gm.subClass({ imageMagick: true });
 const app = express();
 const port = process.env.PORT || 3000;
 
-function getFolderName(width, height, ignore_aspect ){
-  if(!width && !height){
-    return "original";
+function getFolderName(width, height, ignore_aspect) {
+  if (!width && !height) {
+    return 'original';
   }
 
-  if(width & !height){
-    return "w" + width;
+  if (width & !height) {
+    return 'w' + width;
   }
 
-  if(!width & height){
-    return "h" + height;
+  if (!width & height) {
+    return 'h' + height;
   }
 
-  return "w" + width + "h" + height;
+  return 'w' + width + 'h' + height;
 }
 
 app.get('/images/:hash', (req, res) => {
-
   const hash = req.params.hash;
   const width = req.query.w || null;
   const height = req.query.h || null;
-  const ignore_aspect = req.query.ignore_aspect != null ? "!" : null;
+  const ignore_aspect = req.query.ignore_aspect != null ? '!' : null;
 
   const folderName = getFolderName(width, height, ignore_aspect);
 
-  const reqParams = { Bucket: process.env.S3_BUCKET, Key: folderName + '/' + hash };
+  const reqParams = {
+    Bucket: process.env.S3_BUCKET,
+    Key: folderName + '/' + hash,
+  };
 
-  s3.headObject(Object.assign({}, reqParams, {Key: "original" + '/' + hash}) , function(error, originalData){
-    if(error){
-      console.error("image not found");
-      res.status(404).send({ error: 'Not found' });
-    }else{
-      console.info("original image found");
-      console.info("check for image with " + folderName);
-      s3.headObject(reqParams, function(error, data){
-        if(error){
+  s3.headObject(
+    Object.assign({}, reqParams, { Key: 'original' + '/' + hash }),
+    function(error, originalData) {
+      if (error) {
+        console.error('image not found');
+        res.status(404).send({ error: 'Not found' });
+      } else {
+        console.info('original image found');
+        console.info('check for image with ' + folderName);
+        s3.headObject(reqParams, function(error, data) {
+          if (error) {
+            console.info(`image with ${folderName} not found, creating now`);
 
-          console.info("image with " + folderName + "not found, creating now");
+            const s3Stream = s3
+              .getObject(
+                Object.assign({}, reqParams, { Key: 'original' + '/' + hash }),
+              )
+              .createReadStream();
 
-          const s3Stream = s3.getObject(Object.assign({}, reqParams, {Key: "original" + '/' + hash})).createReadStream();
-
-          console.info("shrinking");
-          gmObj(s3Stream)
-          .resize(width, height, ignore_aspect)
-          .toBuffer(function (error, buffer) {
-            if (error){
-              console.error("error uploading image");
-              res.status(500).send({ error: 'Upload error' });
-            }else{
-              console.info("uploading shrunken image");
-              s3.putObject(Object.assign({}, reqParams, { Body: buffer, ContentLength: buffer.length, ContentType: originalData.ContentType, ACL: 'public-read' }), function(error, data){
-                if(error){
-                  console.error("error uploading image");
+            console.info('shrinking');
+            gmObj(s3Stream)
+              .resize(width, height, ignore_aspect)
+              .toBuffer(function(error, buffer) {
+                if (error) {
+                  console.error('error uploading image');
                   res.status(500).send({ error: 'Upload error' });
-                }else{
-                  console.info("sending image response");
-                  res.status(200)
-                  res.end(buffer);
+                } else {
+                  console.info('uploading shrunken image');
+                  s3.putObject(
+                    Object.assign({}, reqParams, {
+                      Body: buffer,
+                      ContentLength: buffer.length,
+                      ContentType: originalData.ContentType,
+                      ACL: 'public-read',
+                    }),
+                    function(error, data) {
+                      if (error) {
+                        console.error('error uploading image');
+                        res.status(500).send({ error: 'Upload error' });
+                      } else {
+                        console.info('sending image response');
+                        res.status(200);
+                        res.end(buffer);
+                      }
+                    },
+                  );
                 }
               });
-            }
-          })
-
-        }else{
-          console.info("shrunken image found: sending image response");
-          s3.getObject(reqParams).createReadStream().pipe(res);
-        }
-      })
-  }});
+          } else {
+            console.info('shrunken image found: sending image response');
+            s3.getObject(reqParams).createReadStream().pipe(res);
+          }
+        });
+      }
+    },
+  );
 });
 
-
-let server = app.listen(port, function () {
+let server = app.listen(port, () => {
   let host = server.address().address;
   let port = server.address().port;
 
