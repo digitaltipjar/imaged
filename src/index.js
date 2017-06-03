04bb8d2d13876a3fce48e6080c09227bc50f43a8
Bryan Joseph <bryanjos@gmail.com>
2017-06-03 05:59:17 -0500

    Using async/await to make function clearer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const AWS = require('aws-sdk');
 const gm = require('gm');
 const fs = require('fs');
 const multer = require('multer');
+const util = require('util');
 
 const upload = multer({ dest: 'uploads/' });
 
@@ -11,6 +12,11 @@ const s3 = new AWS.S3({ region: 'us-east-1' });
 const gmObj = gm.subClass({ imageMagick: true });
 const app = express();
 const port = process.env.PORT || 3000;
+
+//http://blog.grossman.io/how-to-write-async-await-without-try-catch-blocks-in-javascript/
+function to(promise) {
+  return promise.then(data => [null, data]).catch(err => [err, null]);
+}
 
 function getFolderName(width, height, ignore_aspect) {
   if (!width && !height) {
@@ -28,7 +34,7 @@ function getFolderName(width, height, ignore_aspect) {
   return 'w' + width + 'h' + height;
 }
 
-app.get('/images/:hash', (req, res) => {
+app.get('/images/:hash', async (req, res) => {
   const hash = req.params.hash;
   const width = req.query.w || null;
   const height = req.query.h || null;
@@ -41,60 +47,76 @@ app.get('/images/:hash', (req, res) => {
     Key: folderName + '/' + hash,
   };
 
-  s3.headObject(
-    Object.assign({}, reqParams, { Key: 'original' + '/' + hash }),
-    function(error, originalData) {
-      if (error) {
-        console.error('image not found');
-        res.status(404).send({ error: 'Not found' });
-      } else {
-        console.info('original image found');
-        console.info('check for image with ' + folderName);
-        s3.headObject(reqParams, function(error, data) {
-          if (error) {
-            console.info(`image with ${folderName} not found, creating now`);
-            const getObjectConfig = Object.assign({}, reqParams, {
-              Key: 'original' + '/' + hash,
-            });
-            const s3Stream = s3.getObject(getObjectConfig).createReadStream();
+  let error, originalData, resizedData, buffer, data;
 
-            console.info('shrinking');
-            gmObj(s3Stream)
-              .resize(width, height, ignore_aspect)
-              .toBuffer(function(error, buffer) {
-                if (error) {
-                  console.error('error uploading image');
-                  res.status(500).send({ error: 'Upload error' });
-                } else {
-                  console.info('uploading shrunken image');
-                  s3.putObject(
-                    Object.assign({}, reqParams, {
-                      Body: buffer,
-                      ContentLength: buffer.length,
-                      ContentType: originalData.ContentType,
-                      ACL: 'public-read',
-                    }),
-                    function(error, data) {
-                      if (error) {
-                        console.error('error uploading image');
-                        res.status(500).send({ error: 'Upload error' });
-                      } else {
-                        console.info('sending image response');
-                        res.status(200);
-                        res.end(buffer);
-                      }
-                    }
-                  );
-                }
-              });
-          } else {
-            console.info('shrunken image found: sending image response');
-            s3.getObject(reqParams).createReadStream().pipe(res);
-          }
-        });
-      }
-    }
+  const resizedImageExistPromise = s3.headObject(reqParams).promise();
+
+  //Check to see if resized file exists
+  [error, resizedData] = await to(resizedImageExistPromise);
+
+  if (resizedData) {
+    //It does so we can stop here
+    s3.getObject(reqParams).createReadStream().pipe(res);
+    return;
+  }
+
+  const originalImageParams = Object.assign({}, reqParams, {
+    Key: 'original' + '/' + hash,
+  });
+
+  const originalImageExistPromise = s3
+    .headObject(originalImageParams)
+    .promise();
+
+  //Check to see if original file exists
+  [error, originalData] = await to(originalImageExistPromise);
+
+  if (error) {
+    console.error('image not found');
+    res.status(404).send({ error: 'Not found' });
+    return;
+  }
+
+  console.info(`image with ${folderName} not found, creating now`);
+  const getObjectConfig = Object.assign({}, reqParams, {
+    Key: 'original' + '/' + hash,
+  });
+
+  //Let's generate a new resized image now
+  //We get the original image and resize it
+  const s3Stream = s3.getObject(getObjectConfig).createReadStream();
+  const bufferPromise = util.promisify(
+    gmObj(s3Stream).resize(width, height, ignore_aspect).toBuffer
   );
+
+  [error, buffer] = await to(bufferPromise);
+
+  if (error) {
+    console.error('error generating image');
+    res.status(500).send({ error: 'Image generation error' });
+    return;
+  }
+
+  console.info('uploading shrunken image');
+  const putParams = Object.assign({}, reqParams, {
+    Body: buffer,
+    ContentLength: buffer.length,
+    ContentType: originalData.ContentType,
+    ACL: 'public-read',
+  });
+
+  const putResizedImagePromise = s3.putObject(putParams).promise();
+
+  [error, data] = await to(putResizedImagePromise);
+
+  if (error) {
+    console.error('error uploading image');
+    res.status(500).send({ error: 'Upload error' });
+  } else {
+    console.info('sending image response');
+    res.status(200);
+    res.end(buffer);
+  }
 });
 
 let server = app.listen(port, () => {
